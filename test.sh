c5365d8b7b052b0326c8816c2d985d7f44a78059
Manoel Ribeiro Almeida <manoel.ribeiro@unilab.edu.br>
2025-03-26 18:01:17 -0300

    test3

--- test.sh
+++ test.sh
@@ -1,9 +1,13 @@
 git clone https://github.com/idealo/mongodb-performance-test.git
 cp ./mongodb-performance-test/latest-version/mongodb-performance-test.jar test.jar
+host1=localhost
+host2=localhost
+host3=localhost
+replicaset=rs
 
 #jarfile=./mongodb-performance-test/latest-version/mongodb-performance-test.jar
-java -jar test.jar -m insert -o 1000000 -t 10 -db test -c perf 
-java -jar test.jar -m update_one -d 3600 -t 10 20 30 -db test -c perf 
-java -jar test.jar -m update_one iterate_many -d 3600 -t 10 10 20 20 30 30 -db test -c perf 
-java -jar test.jar -m update_one iterate_many -d 3600 -t 10 20 20 40 30 60 -db test -c perf 
-java -jar test.jar -m update_one iterate_many -url mongodb://localhost:27017,host-2:27017,host-3:27017/?replicaSet=<rsName> -d 3600 -t 10 20 20 40 30 60 -db test -c perf 
+java -jar test.jar -m insert -o 1000000 -t 10 -h $host1 -db test -c perf 
+java -jar test.jar -m update_one -d 3600 -t 10 20 30 -h $host1 -db test -c perf 
+java -jar test.jar -m update_one iterate_many -d 3600 -t 10 10 20 20 30 30 -h $host1 -db test -c perf 
+java -jar test.jar -m update_one iterate_many -d 3600 -t 10 20 20 40 30 60 -h $host1 -db test -c perf 
+java -jar test.jar -m update_one iterate_many -url mongodb://$host1:27017,$host2:27017,$host3:27017/?replicaSet=$replicaset -d 3600 -t 10 20 20 40 30 60 -db test -c perf 
