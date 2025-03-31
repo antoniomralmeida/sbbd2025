dad17f66c85c36b3f0cce5e105f2e2923c6583eb
Manoel Ribeiro Almeida <manoel.ribeiro@unilab.edu.br>
2025-03-31 13:35:08 -0300

    cluster

--- test.sh
+++ test.sh
@@ -4,10 +4,11 @@ host1=mongo1:27017
 host2=mongo2:27018
 host3=mongo3:27019
 replicaset=myReplicaSet
+primary=$host3
 
 jarfile=./mongodb-performance-test/latest-version/mongodb-performance-test.jar
-java -jar test.jar -m insert -o 1000000 -t 10 -h $host1 -db test -c perf 
-java -jar test.jar -m update_one -d 3600 -t 10 20 30 -h $host1 -db test -c perf 
-java -jar test.jar -m update_one iterate_many -d 3600 -t 10 10 20 20 30 30 -h $host1 -db test -c perf 
-java -jar test.jar -m update_one iterate_many -d 3600 -t 10 20 20 40 30 60 -h $host1 -db test -c perf 
+java -jar test.jar -m insert -o 1000000 -t 10 -h $primary -db test -c perf 
+java -jar test.jar -m update_one -d 3600 -t 10 20 30 -h $primary -db test -c perf 
+java -jar test.jar -m update_one iterate_many -d 3600 -t 10 10 20 20 30 30 -h $primary -db test -c perf 
+java -jar test.jar -m update_one iterate_many -d 3600 -t 10 20 20 40 30 60 -h $primary -db test -c perf 
 java -jar test.jar -m update_one iterate_many -url mongodb://$host1,$host2,$host3/?replicaSet=$replicaset -d 3600 -t 10 20 20 40 30 60 -db test -c perf 
